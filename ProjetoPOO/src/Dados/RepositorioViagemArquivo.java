7ca87a3645685eaaf982f8c1bc877e25352d25ea
Renato Beltrão <renatinbeltrao@gmail.com>
2025-03-31 19:27:59 -0300

    Fazendo repositorio,gerenciador e interface relacionadas a viagem.

--- ProjetoPOO/src/Dados/RepositorioViagemArquivo.java
+++ ProjetoPOO/src/Dados/RepositorioViagemArquivo.java
@@ -3,12 +3,18 @@ package Dados;
 import Negocio.Basicas.Cidade;
 import Negocio.Basicas.Viagem;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioViagemArquivo implements IRepositorioViagem{
     String arquivo = "ArquivoViagem.ser";
     List<Viagem> viagens;
+
+    RepositorioViagemArquivo() { viagens = carregar(); }
 
     @Override
     public List<Viagem> buscarPorCidade(Cidade cidade) {
@@ -31,13 +37,25 @@ public class RepositorioViagemArquivo implements IRepositorioViagem{
         return this.viagens;
     }
 
+    private List<Viagem> carregar(){
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo))){
+            return (List<Viagem>) ois.readObject();
+        }catch(Exception e){
+            return new ArrayList<>();
+        }
+    }
+
     public void adicionar(Viagem viagem) {
-        this.viagens.add(viagem);
+        viagens.add(viagem);
         salvar();
     }
 
     private void salvar(){
-
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(arquivo))){
+            oos.writeObject(viagens);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 
